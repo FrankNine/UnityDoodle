cf4447c0aeea2d71a7acde5f83fb47c7ffae9c3d
Chun-Fu Chao <aaefiikmnnnr@gmail.com>
2023-06-27 22:48:43 +0800

    Display Sprite in Atlas in EditorWindow

---
+++ Assets/Scripts/Editor/DisplaySpriteInAtlas.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEditor;
+
+public class DisplaySpriteInAtlas : EditorWindow
+{
+    [MenuItem("Window/Doodle/Display Sprite in Atlas")]
+    private static void Init()
+    {
+        // Window Set-Up
+        var window = GetWindow<DisplaySpriteInAtlas>(false, "Display Sprite in Atlas", true);
+        window.position = new Rect(Vector2.zero, new Vector2(300, 300));
+        window.Show();
+    }
+
+    private int _index;
+    private Object[] _objects;
+    
+    public void OnGUI()
+    {
+        // Index 0 is Texture2D
+        _index = EditorGUILayout.IntSlider(_index, 1, 12);
+        
+        if (_objects == null)
+        {
+            // Sprite from: https://opengameart.org/content/green-cap-character-16x18
+            var guid = AssetDatabase.FindAssets("Green-Cap-Character-16x18")[0];
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            _objects = AssetDatabase.LoadAllAssetsAtPath(path);
+        }
+
+        _DrawSprite(new Rect(100, 100, 100, 100), _objects[_index] as Sprite);
+    }
+
+    // Code from: https://forum.unity.com/threads/drawing-a-sprite-in-editor-window.419199/#post-3059891
+    private static void _DrawSprite(Rect position, Sprite sprite)
+    {
+        if (sprite == null) { return; }
+        
+        var textureSize = new Vector2(sprite.texture.width, sprite.texture.height);
+
+        var uv = new Rect
+        (
+            sprite.textureRect.x / textureSize.x,
+            sprite.textureRect.y / textureSize.y,
+            sprite.textureRect.width / textureSize.x,
+            sprite.textureRect.height / textureSize.y
+        );
+
+        var minScale = Mathf.Min
+        (
+            position.width / sprite.textureRect.width,
+            position.height / sprite.textureRect.height
+        );
+
+        var pos = new Rect
+        {
+            x = position.x,
+            y = position.y,
+            width = sprite.textureRect.width * minScale,
+            height = sprite.textureRect.height * minScale
+        };
+        
+        GUI.DrawTextureWithTexCoords(pos, sprite.texture, uv);
+    }
+}
